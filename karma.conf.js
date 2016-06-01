a438d1c92fb7fd73031db8deb660b20c729833f5
RobertCawBrush <Robert.Brush@BrieBug.com>
2016-06-01 12:21:31 -0600

    removed chrome launcher

--- karma.conf.js
+++ karma.conf.js
@@ -16,8 +16,6 @@ module.exports = function(config) {
             // From the CLI. Not used here but interesting
             // chrome setup for travis CI using chromium
             Chrome_travis_ci: {
-                base: 'Chrome',
-                flags: ['--no-sandbox']
             }
         },
         files: [
