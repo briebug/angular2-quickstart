30046cbe21bee3202cf0fefcd66841a14dd9ffad
RobertCawBrush <Robert.Brush@BrieBug.com>
2016-06-01 14:14:43 -0600

    adjusted testing scripts to exit correctly

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,7 @@ module.exports = function(config) {
 
     var appBase   = 'src/client/app/components/';      // transpiled app JS files
     var appAssets ='/base/src/client/app/components/'; // component assets fetched by Angular's compiler
+    var client = 'src/client/';
 
     config.set({
         basePath: '',
@@ -42,12 +43,15 @@ module.exports = function(config) {
             // Angular 2 itself and the testing library
             {pattern: 'node_modules/@angular/**/*.js', included: false, watched: false},
             {pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: false},
+            
 
             {pattern: 'systemjs.config.js', included: false, watched: false},
             'karma-test-shim.js',
 
             // transpiled application & spec code paths loaded via module imports
             {pattern: appBase + '**/*.js', included: false, watched: true},
+            {pattern: client + 'test-helpers/*.js', included: false, watched: true},
+
 
             // paths for debugging with source maps in dev tools
             {pattern: appBase + '**/*.ts', included: false, watched: false},
@@ -79,6 +83,6 @@ module.exports = function(config) {
         logLevel: config.LOG_INFO,
         autoWatch: true,
         browsers: ['PhantomJS'],
-        singleRun: true
+        singleRun: false
     })
 }
